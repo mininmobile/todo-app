f425acb0c345350cbb79338ec7b16883264c7a3c
sophie <zvava@disroot.org>
2022-06-24 15:13:16 +0200

    implement BEM :)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,25 @@ import "./Navbar.css";
 
 const Navbar: React.FC = () => {
 	return (
-		<nav>
-			<Link to="/">Todo App</Link>
-			<ul>
-				<NavLink end to="/" className={({ isActive }) => isActive ? "active" : "" }>Home</NavLink>
-				<NavLink end to="/about" className={({ isActive }) => isActive ? "active" : "" }>About</NavLink>
-			</ul>
+		<nav className="navbar">
+			<Link to="/" className="navbar__title">Todo App</Link>
+			<div className="navbar__links">
+				<NavigationLink to="/">Home</NavigationLink>
+				<NavigationLink to="/about">About</NavigationLink>
+			</div>
 		</nav>
+	);
+}
+
+const NavigationLink: React.FC<{ to: string, children: string }> = ({ to, children }) => {
+	return (
+		<NavLink
+			end to={to}
+			className={({ isActive }) =>
+				isActive ? "navbar__link navbar__link__active" : "navbar__link" }
+		>
+			{children}
+		</NavLink>
 	);
 }
 
